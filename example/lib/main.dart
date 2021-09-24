40d69ab17fadf0b8a63eed718ae1f51958beffb7
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2021-09-24 15:01:14 -0300

    update changelog

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GifView Demo'),
     );
   }
 }
